7b2c67a52ce010da0425895682ff0652d8f5fa2a
rsankar9 <remya66669@hotmail.com>
2017-08-10 18:54:25 +0200

    Tried binary representations

--- HTM_modified.py
+++ HTM_modified.py
@@ -3,225 +3,190 @@
 import numpy as np
 
 
-n=21	# no. of columns in a layer
-m=6		# no. of cells per column
-d=20	# no. of distal segments per cell
-s=16	# no. of potential synapses per segment
+n=21				# no. of columns in a layer
+m=6					# no. of cells per column
+d=4					# no. of distal segments per cell
+s=16				# no. of potential synapses per segment
 
 beta = 0.5
 theta = 3
-synPerm = 0.21
+synPerm = 0.21		# initial synaptic permanence
 
 pPos = 0.8
 pNeg = 0.2
-pDecay = 0.008
+pDec = 0.008
 
 
 # ---- Step 1: Initialisation ---- #
-A = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])
-P = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])
+A = np.zeros((n), dtype = [("t", int), ("t-1", int)])
+P = np.zeros((n), dtype = [("t", int), ("t-1", int)])
 D = np.zeros((m, n, d, s), dtype = [("x", int), ("y", int), ("cw", float)])
+Dnew = np.zeros((m, n, d, s), dtype = [("x", int), ("y", int), ("cw", float)])
 
 for i, j, k in np.ndindex(m, n, d):								# initialising each distal segment with synapses to random cells with random strength
 	for l in np.ndindex(s):
 		x = np.random.random_integers(0, m-1)
 		y = np.random.random_integers(0, n-1)
-	#	cw = np.random.random()
+		cw = np.random.random()
 		while x == i and y==j:
 			x = np.random.random_integers(0, m-1)
 			y = np.random.random_integers(0, n-1)
-	#	while cw == 0:
-	#		cw = np.random.random()
+		while cw == 0:
+			cw = np.random.random()
 		D[i][j][k][l]["x"] = x
 		D[i][j][k][l]["y"] = y
-		D[i][j][k][l]["cw"] = synPerm
+		D[i][j][k][l]["cw"] = cw		#	synPerm
 
-
-
-
-#S = {   "A" : np.array([1,0,0,1]),
- #       "B" : np.array([0,0,1,0]),
-  #      "C" : np.array([0,1,0,1])}
-#S = { "A" : np.array([1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]),
-#      "B" : np.array([0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1]),
-#      "C" : np.array([0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0]), 
-#      "D" : np.array([0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0]), 
-#      "X" : np.array([0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0]), 
-#      "Y" : np.array([1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1]), 
-#      "end" : np.array([0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0])  }	# encoding for each syllable
-
-S = { "A" : np.array([0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0]),
+S = { "A" : np.array([0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]),
       "B" : np.array([0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0]),
       "C" : np.array([0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0]), 
-      "D" : np.array([0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0]), 
-      "X" : np.array([0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0]), 
+      "D" : np.array([0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0]), 
+      "X" : np.array([0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0]), 
       "Y" : np.array([1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1]), 
-      "end" : np.array([0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0])  }	# encoding for each syllable
+      "end" : np.array([0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0])  }	# encoding for each syllable
 
 
-W = np.zeros((n), dtype = int)
-
-#print "P", P['t']
-#print "A", A['t']
+W = np.zeros((n), dtype = int)								# feed-forward input representing a syllable
 
 seq1 = ["A", "B", "C", "D", "end"]
 seq2 = ["X", "B", "C", "Y", "end"]
 
-sequences = [seq1, seq2]
+sequences = [seq1, seq2]									# list of sequences to be trained on
+
+def isSegmentActive(i, j, k, D, Mat):						# if no. of connected synapses to active cells > theta in any distal segment of current cell
+	count = 0
+	for l in np.ndindex(s):
+ 		synapse = D[i][j][k][l]
+		if synapse["cw"] > beta and (Mat[synapse["y"]] & (1<<synapse["x"]))!=0:
+			count = count + 1
+	return count > theta
+
+def count_positive_synapses(Mat, Segment):
+	count = 0
+	for l in np.ndindex(s):
+		synapse = Segment[l]
+		if synapse["cw"] > 0 and (Mat[synapse["y"]] & (1<<synapse["x"]))!=0:
+			count = count + 1
+	return count
 
 def reinforce(x, y, z, D, Dnew):
-    delta = 0
-    for l in np.ndindex(s):
- 		syn = D[x][y][z][l]
- 		if syn["cw"] > 0:
- 			delta = pPos * A['t-1'][syn["x"]][syn["y"]] - pNeg 
-		#	s["cw"] = s["cw"] + delta
-		Dnew[x][y][z][l] = syn
-		Dnew[x][y][z][l]["cw"] = syn["cw"] + delta
-			    #delta = pPos * Dpos[i][j][k] * A['t-1'] - pNeg * Dpos[i][j][k]
-
+	for l in np.ndindex(s):
+		synapseOld = D[x][y][z][l]
+		synapseNew = 0
+		delta = 0
+		if A['t-1'][synapseOld["y"]] & (1<<synapseOld["x"]):
+			delta = delta + pPos							# reinforcing synapses to active cells
+		#else:
+		#	delta = delta - pNeg							# negatively reinforcing synapses to inactive cells
+		synapseNew = synapseOld["cw"] + delta
+		if synapseNew < 0:
+			synapseNew = 0
+		if synapseNew > 1:
+			synapseNew = 1
+		Dnew[x][y][z][l] = synapseOld
+		Dnew[x][y][z][l]["cw"] = synapseNew
+		#temp = synapseOld["cw"] - Dnew[x][y][z][l]["cw"]
+		#if temp!=0:
+		#	print "reinforced ", temp
 
 def decay(x, y, z, D, Dnew):
-    delta = 0
-    #for s in D[x][y][z]:
-    for l in np.ndindex(s):
-    	syn = D[x][y][z][l]
-    	if syn["cw"] > 0:
-    		delta = pDecay 
-		Dnew[x][y][z][l] = syn
-		Dnew[x][y][z][l]["cw"] = syn["cw"] - delta
-			    #delta = pDecay * Dpos[i][j][k]
-
-
-def count_ones_in_product_Dcon(G, H):
-	count = 0
-	for syn in H:
-		if syn["cw"] > beta and G[syn["x"], syn["y"]] == 1:
-			count = count + 1
-	return count
-
-def count_ones_in_product_Dpos(G, H):
-	count = 0
-	for syn in H:
-		if syn["cw"] > 0 and G[syn["x"], syn["y"]] == 1:
-			count = count + 1
-	return count
+	#print "for decay"
+	for l in np.ndindex(s):
+		synapseOld = D[x][y][z][l]
+		synapseNew = Dnew[x][y][z][l]
+		#if synapseOld["cw"] > 0:
+		synapseNew["cw"] = synapseOld["cw"] - pDec
+		if synapseNew["cw"] < 0:
+			synapseNew["cw"] = 0
+		if synapseNew["cw"] > 1:
+			synapseNew["cw"] = 1
+		Dnew[x][y][z][l] = synapseNew
+		#temp = synapseOld["cw"] - synapseNew["cw"]
+		#if temp!=0:
+		#	print "decayed ", temp
+	
 
 for seq in sequences:
 	for ntrials in range(500):
 	# ---- Step 2: Computing cell states ---- #
 		for syllable in seq:
-			
 			W = S[syllable]									# marks winning columns
 			A['t'] = 0
+			P['t'] = 0
 			active_cells = []
 			for j in np.ndindex(n):
+				A['t'][j] = 0
+				P['t'][j] = 0
 				if W[j] == 1:
-					found_active = 0
-					for i in np.ndindex(m):
-						if P['t-1'][i][j] == 1:
-							A['t'][i][j] = 1					# cell activated if present in winning column and if predicted previously
-							found_active = 1
-							active_cells.insert(0, (i,j))
-					if found_active==0:
-						for i in np.ndindex(m):
-							A['t'][i][j] = 1				# cell activated if present in winning column and if no cell in the column had been predicted
+					if P['t-1'][j] != 0:
+						A['t'][j] = P['t-1'][j]			# cell activated if present in winning column and if predicted previously
+					else:
+						A['t'][j] = pow(2,m)-1				# cell activated if present in winning column and if no cell in the column had been predicted
+					
+					for i in range(m):
+						if (A['t'][j] & (1 << i)) != 0:
 							active_cells.insert(0, (i,j))
 		    		
-			for i, j in np.ndindex(m, n):					# computing predictive state for this time step
-			    P['t'][i][j] = 0
-			    for q in np.ndindex(d):
-			    	if count_ones_in_product_Dcon(A['t'], D[i][j][q]) > theta:
-			        #if sum(sum(A['t']*Dcon[i][j][q])) > theta:
-			            P['t'][i][j] = 1					# if no. of connected synapses to active cells > theta in any distal segment of current cell
-			            break
-			            
+			for i,j in np.ndindex(m,n):						# computing predictive state for this time step
+			    for k in np.ndindex(d):
+			    	if isSegmentActive(i, j, k, D, A['t']) == True:
+			    		P['t'][j] = P['t'][j] | 1<<i
+			    		break
 			
 			# ---- Step 3: Learning ---- #
-			#Dpos = D > 0
-
 			Dnew = np.zeros((m, n, d, s), dtype = [("x", int), ("y", int), ("cw", float)])
-			for i, j, k in np.ndindex(m, n, d):				# active dendritic segment reinforced if cell predicted correctly
+			for j in np.ndindex(n):							# active dendritic segment reinforced if cell predicted correctly
 			    if W[j] == 1:
-			    	if P['t-1'][i][j] > 0 and count_ones_in_product_Dcon(A['t-1'], D[i][j][k]) > theta:
-			        #if P['t-1'][i][j] > 0 and sum(sum(Dcon[i][j][k] * A['t-1'])) > theta:
-			        	reinforce(i, j, k, D, Dnew)
+			    	if P['t-1'][j] != 0:
+				    	for i in range(m):
+				    		if (P['t-1'][j] & (1<<i) != 0) and isSegmentActive(i, j, k, D, A['t-1']) == True:
+				        		reinforce(i, j, k, D, Dnew)
 
-			for j in np.ndindex(n):						
-			    if W[j] == 1:
-			    	tempSum = 0
-			    	for i in np.ndindex(m):				# to check if current winning column was predicted
-			    		tempSum += P['t-1'][i][j]
-
-			    	if tempSum == 0:					# if current winning column not predicted
+			    	else:									# if current winning column not predicted
 			    		maxSum = 0
-				    	for i, k in np.ndindex(m, d):	# to find distal segment in current column closest to activation
-				    		currSum = count_ones_in_product_Dpos(A['t-1'], D[i][j][k])
-				    	#	currSum = sum(sum(Dpos[i][j][k] * A['t-1']))
-				    		maxSum = max(maxSum, currSum)
+			    		maxRow = 0
+			    		maxSeg = 0
+				    	for i, k in np.ndindex(m, d):		# to find distal segment in current column closest to activation
+				    		maxSum = max(maxSum, count_positive_synapses(A['t-1'], D[i][j][k]))
+				    		maxRow = i
+				    		maxSeg = k
+				    	reinforce(maxRow, j, maxSeg, D, Dnew)
+
 				        for i, k in np.ndindex(m, d):
-				        	if count_ones_in_product_Dpos(A['t-1'], D[i][j][k]) == maxSum:
-					    #    if sum(sum(Dpos[i][j][k] * A['t-1'])) == maxSum:
-					        	reinforce(i, j, k, D, Dnew)
-					        else:							# no match found, hence updating synapses
+
+				        	if i!=maxRow or k!=maxSeg:						# no match found, hence updating synapses
 								list_active = active_cells	
 								np.random.shuffle(list_active)
+
 								for l in np.ndindex(s):
-									if D[i][j][k][l]["cw"]<beta:
-										if len(list_active)==0:		
-											break
+									if len(list_active)==0:		
+										break
+									if D[i][j][k][l]["cw"]<beta:			# maybe it's needed for some other syllable if cw > beta
 										r = np.random.randint(0, m*n)
 										pos = r%len(list_active)			# randomly pick an active cell
 										Dnew[i][j][k][l]["x"] = list_active[pos][0]		# replace existing synapse with synapse to active cell
 										Dnew[i][j][k][l]["y"] = list_active[pos][1]
-									#	Dnew[i][j][k][l]["cw"] = beta + np.random.random()*(1-beta)	# to ensure it has a chance to become a strong connection
-										Dnew[i][j][k][l]["cw"] = synPerm	
+										Dnew[i][j][k][l]["cw"] = beta + np.random.random()*(1-beta)	# synPerm	# to ensure it has a chance to become a strong connection
 										list_active.pop(pos)
 
-			for i, j, k in np.ndindex(m, n, d):			# to negatively reinforce active segments of inactive cells
-				if A['t'][i][j] == 0 and count_ones_in_product_Dcon(A['t-1'], D[i][j][k]) > theta:
-			#	if A['t'][i][j] == 0 and sum(sum(Dcon[i][j][k] * A['t-1'])) > theta:
-					decay(i, j, k, D, Dnew)
+			for j in range(n):												# to negatively reinforce active segments of inactive cells
+				if A['t'][j] == 0:
+					for i, k in np.ndindex(m, d):
+						if isSegmentActive(i, j, k, D, A['t-1']) == True:
+							decay(i, j, k, D, Dnew)
 			
-
-
-			out = "None"
-			outputW = np.zeros(n, dtype=int)
-			for j in np.ndindex(n):
-				column_active = 0
-				for i in np.ndindex(m):
-					if A['t'][i][j]==1:
-						column_active = 1
-						outputW[j] = 1
-						break
+			out = ""
+			outputW = A['t'] > 0
 				
 			for syll in seq:
-				flag = 0
-				for j in np.ndindex(n):
-					if S[syll][j] != outputW[j]:
-						flag = 1
-						break
-				if flag == 0:
-					out = syll
-					break
+				if ((outputW & S[syll]) == S[syll]).all():
+					out = out + syll
 
 			pred = ""
-			predW = np.zeros(n, dtype=int)
-			for j in np.ndindex(n):
-				column_active = 0
-				for i in np.ndindex(m):
-					if P['t-1'][i][j]==1:
-						column_active = 1
-						predW[j] = 1
-						break
+			predW = P['t-1'] > 0
 				
 			for syll in seq:
-				flag = 0
-				for j in np.ndindex(n):
-					if S[syll][j] == 1 and predW[j]!=1:
-						flag = 1
-						break
-				if flag == 0:
+				if ((predW & S[syll]) == S[syll]).all():
 					pred = pred+syll
 
 			if (ntrials+1)%100==0:		
@@ -233,16 +198,16 @@ for seq in sequences:
 				print "output syllable: ", out
 				print A['t']
 	
-
 			D = Dnew
 			P['t-1'] = P['t']
 			A['t-1'] = A['t']
 			A['t'] = 0
+			P['t'] = 0
 
 
 		# reset on encountering "end" syllable
-		P['t-1'] = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])	
-		A['t-1'] = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])
+		P['t-1'] = np.zeros((n), dtype = [("t", int), ("t-1", int)])	
+		A['t-1'] = np.zeros((n), dtype = [("t", int), ("t-1", int)])
 
 print "Testing"
 
@@ -255,62 +220,39 @@ for test in tests:
 	for syllable in test:
 		W = S[syllable]									# marks winning columns
 		A['t'] = 0
+		P['t'] = 0
+		active_cells = []
 		for j in np.ndindex(n):
+			A['t'][j] = 0
+			P['t'][j] = 0
 			if W[j] == 1:
-				found_active = 0
-				for i in np.ndindex(m):
-					if P['t-1'][i][j] == 1:
-						A['t'][i][j] = 1					# cell activated if present in winning column and if predicted previously
-						found_active = 0
-				if found_active==0:
-					for i in np.ndindex(m):
-						A['t'][i][j] = 1				# cell activated if present in winning column and if no cell in the column had been predicted
+				if P['t-1'][j] != 0:
+					A['t'][j] = P['t-1'][j]			# cell activated if present in winning column and if predicted previously
+				else:
+					A['t'][j] = pow(2,m)-1				# cell activated if present in winning column and if no cell in the column had been predicted
+				
+				for i in range(m):
+					if (A['t'][j] & (1 << i)) != 0:
 						active_cells.insert(0, (i,j))
 	    		
-		for i, j in np.ndindex(m, n):					# computing predictive state for this time step
-		    P['t'][i][j] = 0
-		    for q in np.ndindex(d):
-		    	if count_ones_in_product_Dcon(A['t'], D[i][j][q]) > theta:
-		        #if sum(sum(A['t']*Dcon[i][j][q])) > theta:
-		            P['t'][i][j] = 1					# if no. of connected synapses to active cells > theta in any distal segment of current cell
+		for i,j in np.ndindex(m,n):						# computing predictive state for this time step
+		    for k in np.ndindex(d):
+		    	if isSegmentActive(i, j, k, D, A['t']) == True:
+		    		P['t'][j] = P['t'][j] | 1<<i
+		    		break
 
-		out = "None"
-		outputW = np.zeros(n, dtype=int)
-		for j in np.ndindex(n):
-			column_active = 0
-			for i in np.ndindex(m):
-				if A['t'][i][j]==1:
-					column_active = 1
-					outputW[j] = 1
-					break
+		out = ""
+		outputW = A['t'] > 0
 			
 		for syll in seq:
-			flag = 0
-			for j in np.ndindex(n):
-				if S[syll][j] != outputW[j]:
-					flag = 1
-					break
-			if flag == 0:
-				out = syll
-				break
+			if ((outputW & S[syll]) == S[syll]).all():
+				out = out + syll
 
 		pred = ""
-		predW = np.zeros(n, dtype=int)
-		for j in np.ndindex(n):
-			column_active = 0
-			for i in np.ndindex(m):
-				if P['t'][i][j]==1:
-					column_active = 1
-					predW[j] = 1
-					break
+		predW = P['t'] > 0
 			
 		for syll in seq:
-			flag = 0
-			for j in np.ndindex(n):
-				if S[syll][j] == 1 and predW[j]!=1:
-					flag = 1
-					break
-			if flag == 0:
+			if ((predW & S[syll]) == S[syll]).all():
 				pred = pred+syll
 
 		print "input syllable: ", syllable
@@ -320,13 +262,13 @@ for test in tests:
 		print "output syllable: ", out
 		print A['t']
 
+
 		P['t-1'] = P['t']
 		A['t-1'] = A['t']
 		A['t'] = 0
-
-	P['t-1'] = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])	
-	A['t-1'] = np.zeros((m, n), dtype = [("t", int), ("t-1", int)])
-			            
-
-
+		P['t'] = 0
+	
+	P['t-1'] = np.zeros((n), dtype = [("t", int), ("t-1", int)])	
+	A['t-1'] = np.zeros((n), dtype = [("t", int), ("t-1", int)])
+		
 print "ERR"
