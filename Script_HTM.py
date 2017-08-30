13ffe041bc65e67cd8ec2b67f120cf00814d4cca
rsankar9 <remya66669@hotmail.com>
2017-08-30 11:03:28 +0200

    Update Script_HTM.py

--- Script_HTM.py
+++ Script_HTM.py
@@ -1,11 +1,22 @@
 import HTM_diff
 
-count = 1
-for d in range(1, 3):
-	for s in range(3, 5):
-	#	for nSR in [0, 5, 10, 15, 20]:
-		for nSR in [10]:
-			for nTT in [50]:
-				resFile = "Run" + str(count) + ".json"
-				HTM_diff.HTM(d, s, nSR, nTT, resFile)
-				count = count + 1
+file_count = 1
+f = open('got1','w')
+g = open('got2','w')
+h = open('gotBoth','w')
+for d in [10, 15, 20]:
+	for s in range(13,16):
+		for nSR in [10, 15]:
+			for nTT in [50, 100]:
+				resFile = "Run" + str(file_count) + ".json"
+				flag1, flag2, flagBoth = HTM_diff.HTM(d, s, nSR, nTT, resFile)
+				if flag1 == True:
+					f.write(resFile+'\n')
+				if flag2 == True:
+					g.write(resFile+'\n')
+				if flagBoth == True:
+					h.write(resFile+'\n')	
+				file_count = file_count + 1
+f.close()
+g.close()
+h.close()
