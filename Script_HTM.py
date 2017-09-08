c6e60fd6c408e2584cfe57e324230f5c557866e6
rsankar9 <remya66669@hotmail.com>
2017-09-08 18:33:36 +0200

    Update Script_HTM.py

--- Script_HTM.py
+++ Script_HTM.py
@@ -1,22 +1,23 @@
 import HTM_diff
 
 file_count = 1
-f = open('got1','w')
-g = open('got2','w')
-h = open('gotBoth','w')
-for d in [10, 15, 20]:
-	for s in range(13,16):
-		for nSR in [10, 15]:
-			for nTT in [50, 100]:
-				resFile = "Run" + str(file_count) + ".json"
-				flag1, flag2, flagBoth = HTM_diff.HTM(d, s, nSR, nTT, resFile)
-				if flag1 == True:
-					f.write(resFile+'\n')
-				if flag2 == True:
-					g.write(resFile+'\n')
-				if flagBoth == True:
-					h.write(resFile+'\n')	
-				file_count = file_count + 1
+
+# resFile = "WithCondRun" + str(file_count) + ".json"
+# HTM_diff.HTM(resFile)
+f = open('gotBoth','w')
+f.write('resFile		d	s	nRC\n')	
+for d in [1,2,5]:
+    for s in [5,7,9]:
+        for nRC in [5,10]:
+            resFile = "WithoutCondRun" + str(file_count) + ".json"
+            flagBoth = HTM_diff.HTM(resFile, s, d, nRC)
+            if flagBoth == True:
+                f.write(resFile+'\t'+str(d)+'\t'+str(s)+'\t'+str(nRC) +'\n')
+            file_count = file_count + 1
+
 f.close()
-g.close()
-h.close()
+
+# for s in [5,8]:
+# 	for file_count in range(1,10):
+# 	    resFile = "WithoutCondRun" + str(file_count) + '_s' + str(s) + ".json"
+# 	    HTM_diff.HTM(resFile, s)
