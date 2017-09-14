61660ef54b3eaecb598681580948c976f218a41d
rsankar9 <remya66669@hotmail.com>
2017-09-14 11:36:43 +0200

    Update Script_HTM.py

--- Script_HTM.py
+++ Script_HTM.py
@@ -1,23 +1,19 @@
-import HTM_diff
+import HTM_binary_s
 
+# file_count = 1
+# f = open('gotBoth','w')
+# f.write('resFile		d	nTT	\n')	
+# for d in [2,5]:
+# 	for repeat in range(10):
+# 		resFile = "WithoutCondRun" + str(file_count) + ".json"
+# 		flagBoth = HTM_binary_s.HTM(resFile, d, d*150)
+# 		if flagBoth == True:
+# 			f.write(resFile + '\t' + str(d) + '\t' + str(d*150) + '\n')	
+# 		file_count = file_count + 1
+
+# f.close()
+
+# For a single run:
 file_count = 1
-
-# resFile = "WithCondRun" + str(file_count) + ".json"
-# HTM_diff.HTM(resFile)
-f = open('gotBoth','w')
-f.write('resFile		d	s	nRC\n')	
-for d in [1,2,5]:
-    for s in [5,7,9]:
-        for nRC in [5,10]:
-            resFile = "WithoutCondRun" + str(file_count) + ".json"
-            flagBoth = HTM_diff.HTM(resFile, s, d, nRC)
-            if flagBoth == True:
-                f.write(resFile+'\t'+str(d)+'\t'+str(s)+'\t'+str(nRC) +'\n')
-            file_count = file_count + 1
-
-f.close()
-
-# for s in [5,8]:
-# 	for file_count in range(1,10):
-# 	    resFile = "WithoutCondRun" + str(file_count) + '_s' + str(s) + ".json"
-# 	    HTM_diff.HTM(resFile, s)
+resFile = "Run" + str(file_count) + ".json"
+HTM_binary_s.HTM(resFile)
